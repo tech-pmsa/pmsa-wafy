41c4ee668a9db5d429a1bdb9baac31ac627918b4
msalihkc <codewithsalih@gmail.com>
2025-07-29 10:42:41 +0530

    officer dashboard

--- app/admins/officer/officer-dashboard/page.tsx
+++ app/admins/officer/officer-dashboard/page.tsx
@@ -1,49 +1,37 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 function page() {
-  return (
-    <div>officer</div>
-  )
+  const supabase = createClientComponentClient();
+
+  const [user, setUser] = useState<{ name: string; role: string } | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) return;
+
+      const { data: profile } = await supabase
+        .from("profiles")
+        .select("name, role")
+        .eq("uid", user.id)
+        .single();
+
+      if (profile) {
+        setUser(profile);
+      }
+    };
+
+    fetchUser();
+  }, [supabase]);
+
+  if (!user) return <p className="text-center mt-10">Loading...</p>;
+
+  return <div>{user.role} Login</div>;
 }
 
-export default page
-
-// import React, { useEffect, useState } from "react";
-// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-// function page() {
-//   const supabase = createClientComponentClient();
-
-//   const [user, setUser] = useState<{ name: string; role: string } | null>(null);
-
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       const {
-//         data: { user },
-//       } = await supabase.auth.getUser();
-//       if (!user) return;
-
-//       const { data: profile } = await supabase
-//         .from("profiles")
-//         .select("name, role")
-//         .eq("uid", user.id)
-//         .single();
-
-//       if (!profile) {
-//         const { data: student } = await supabase
-//           .from("students")
-//           .select("name, role")
-//           .eq("uid", user.id)
-//           .single();
-//         if (student) setUser(student);
-//       } else {
-//         setUser(profile);
-//       }
-//     };
-
-//     fetchUser();
-//   }, [supabase]);
-//   return <div>officer</div>;
-// }
-
-// export default page;
+export default page;
